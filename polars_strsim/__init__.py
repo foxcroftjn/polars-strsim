d8cea931266c0352187bab5959c69753a64e0cd1
Jeremy Foxcroft <foxcroftjn@gmail.com>
2024-06-01 13:54:03 -0400

    Refactor for PyPI distribution

--- polars_strsim/__init__.py
+++ polars_strsim/__init__.py
@@ -1,71 +1,64 @@
-from __future__ import annotations
-
 from pathlib import Path
-from typing import TYPE_CHECKING
-
 import polars as pl
+from polars.plugins import register_plugin_function
+from polars._typing import IntoExpr
+from polars_strsim.utils import parse_into_expr
 
-from polars_strsim.utils import parse_into_expr, register_plugin, parse_version
-
-if TYPE_CHECKING:
-    from polars.type_aliases import IntoExpr
-
-if parse_version(pl.__version__) < parse_version("0.20.16"):
-    from polars.utils.udfs import _get_shared_lib_location
-
-    lib: str | Path = _get_shared_lib_location(__file__)
-else:
-    lib = Path(__file__).parent
 
 def levenshtein(expr: IntoExpr, other: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr, dtype=pl.Utf8)
     other = parse_into_expr(other, dtype=pl.Utf8)
-    return expr.register_plugin(
-        lib=lib,
-        args=[other],
-        symbol="levenshtein",
+    return register_plugin_function(
+        plugin_path=Path(__file__).parent,
+        function_name="levenshtein",
+        args=[expr, other],
         is_elementwise=True,
     )
+
 
 def jaro(expr: IntoExpr, other: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr, dtype=pl.Utf8)
     other = parse_into_expr(other, dtype=pl.Utf8)
-    return expr.register_plugin(
-        lib=lib,
-        args=[other],
-        symbol="jaro",
+    return register_plugin_function(
+        plugin_path=Path(__file__).parent,
+        function_name="jaro",
+        args=[expr, other],
         is_elementwise=True,
     )
+
 
 def jaro_winkler(expr: IntoExpr, other: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr, dtype=pl.Utf8)
     other = parse_into_expr(other, dtype=pl.Utf8)
-    return expr.register_plugin(
-        lib=lib,
-        args=[other],
-        symbol="jaro_winkler",
+    return register_plugin_function(
+        plugin_path=Path(__file__).parent,
+        function_name="jaro_winkler",
+        args=[expr, other],
         is_elementwise=True,
     )
+
 
 def jaccard(expr: IntoExpr, other: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr, dtype=pl.Utf8)
     other = parse_into_expr(other, dtype=pl.Utf8)
-    return expr.register_plugin(
-        lib=lib,
-        args=[other],
-        symbol="jaccard",
+    return register_plugin_function(
+        plugin_path=Path(__file__).parent,
+        function_name="jaccard",
+        args=[expr, other],
         is_elementwise=True,
     )
+
 
 def sorensen_dice(expr: IntoExpr, other: IntoExpr) -> pl.Expr:
     expr = parse_into_expr(expr, dtype=pl.Utf8)
     other = parse_into_expr(other, dtype=pl.Utf8)
-    return expr.register_plugin(
-        lib=lib,
-        args=[other],
-        symbol="sorensen_dice",
+    return register_plugin_function(
+        plugin_path=Path(__file__).parent,
+        function_name="sorensen_dice",
+        args=[expr, other],
         is_elementwise=True,
     )
+
 
 __all__ = [
     "levenshtein",
